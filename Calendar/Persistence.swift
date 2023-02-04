eb0b052cf259e7750a5d606739148e8fc526f1aa
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2023-02-04 14:58:34 +0530

    shared app group created for both app and widget

--- Calendar/Persistence.swift
+++ Calendar/Persistence.swift
@@ -9,6 +9,11 @@ import CoreData
 
 struct PersistenceController {
     static let shared = PersistenceController()
+    
+    var sharedStoreURL: URL {
+        let container = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.com.castleCode.Calendar")!
+        return container.appendingPathComponent("Calendar.sqlite")
+    }
 
     static var preview: PersistenceController = {
         let result = PersistenceController(inMemory: true)
@@ -36,14 +41,20 @@ struct PersistenceController {
 
     init(inMemory: Bool = false) {
         container = NSPersistentContainer(name: "Calendar")
+        
         if inMemory {
             container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        } else {
+            container.persistentStoreDescriptions.first!.url = sharedStoreURL
+
         }
+        
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
+        
         container.viewContext.automaticallyMergesChangesFromParent = true
     }
 }
